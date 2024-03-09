061277b810140343396c17ebd55f38a9fdde9504
arielmell0 <arielmello@rede.ulbra.br>
2024-03-08 21:31:09 -0300

    feat: add more variants to NDIS card with date

--- app/components/ui/cardNDISWithDate/index.tsx
+++ app/components/ui/cardNDISWithDate/index.tsx
@@ -1,19 +1,25 @@
 import React from 'react'
-import { CardContent, Typography, CardActions, Button, Box, Card } from "@mui/material"
+import { CardContent, Typography, CardActions, Button, Box, Card, IconButton } from "@mui/material"
 import { RightArrowIconBlue } from 'app/components/icons/right-arrow-icon/blue'
+import { RightArrowIconWhite } from 'app/components/icons/right-arrow-icon/white'
 
 interface ICardNDISWithDate {
   category: string,
   date: string,
   title: string,
   text: string,
-  variant: 'major' | 'minor'
+  variant: 'major' | 'minor' | 'blue' | 'outlined'
 }
 
 const CardNDISWithDate = ({ category, date, title, text, variant }: ICardNDISWithDate) => {
+  const color = (variant === 'blue' ? '#FFFFFF' : '#000000')
+  const arrowIcon = (variant === 'blue' ? <RightArrowIconWhite /> : <RightArrowIconBlue />)
+  const backgroundColor = (variant === 'blue' ? '#056FD9' : '#FFFFFF')
+  const renderOrangeBox = (variant === 'major' || variant === 'minor' ? true : false)
+
   return (
-    <Card sx={{ width: variant === 'major' ? '368px' : '343px', backgroundColor: '#FFFFFF', border: '1px solid #CDD3D6' }}>
-      <Box sx={{ width: '100%', height: '4px', backgroundColor: '#E3B505' }} />
+    <Card sx={{ color, width: variant === 'major' ? '368px' : '343px', backgroundColor, border: '1px solid #CDD3D6' }}>
+      {renderOrangeBox && <Box sx={{ width: '100%', height: '4px', backgroundColor: '#E3B505' }} />}
       <CardContent sx={{ padding: variant === 'major' ? '32px 32px 16px 32px' : '16px 16px 8px 16px' }}>
         <Typography sx={{ fontSize: 16, fontWeight: 600, marginBottom: '8px' }} component="div">
           {category}
@@ -21,7 +27,7 @@ const CardNDISWithDate = ({ category, date, title, text, variant }: ICardNDISWit
         <Typography sx={{ fontSize: 14, marginBottom: '16px' }} component="div">
           {date}
         </Typography>
-        <Typography sx={{ fontSize: 20, fontWeight: 600, marginBottom: '16px' }} color="#22272B">
+        <Typography sx={{ fontSize: 20, fontWeight: 600, marginBottom: '16px' }}>
           {title}
         </Typography>
         <Typography sx={{ fontSize: 16 }} component="div">
@@ -29,7 +35,9 @@ const CardNDISWithDate = ({ category, date, title, text, variant }: ICardNDISWit
         </Typography>
       </CardContent>
       <CardActions sx={{ padding: variant === 'major' ? '32px' : '16px', paddingBottom: '16px' }}>
-        <Button size="large" startIcon={<RightArrowIconBlue />} />
+        <IconButton>
+          {arrowIcon}
+        </IconButton>
       </CardActions>
     </Card>
   )
