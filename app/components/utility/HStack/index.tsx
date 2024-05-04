44ddbccac10f45fda9d8fbd2eb6b560262f73025
arielmell0 <arielmello@rede.ulbra.br>
2024-05-04 14:53:55 -0300

    feat: hstack and vstack type

--- app/components/utility/HStack/index.tsx
+++ app/components/utility/HStack/index.tsx
@@ -1,14 +1,21 @@
 import { Stack, StackTypeMap } from "@mui/material"
 import { OverridableComponent } from "@mui/material/OverridableComponent"
+import { ReactNode } from "react"
 
-const HStack = ({ ...props }: OverridableComponent<StackTypeMap<{}, "div">>) => {
+type HStackProps = OverridableComponent<StackTypeMap<{}, "div">> & {
+  children?: ReactNode
+}
+
+const HStack = ({ children, ...props }: HStackProps) => (
   <Stack
     display={'flex'}
     flexDirection={'row'}
     alignItems={'center'}
     justifyContent={'center'}
     {...props}
-  />
-}
+  >
+    {children}
+  </Stack>
+)
 
 export default HStack
--- app/components/utility/VStack/index.tsx
+++ app/components/utility/VStack/index.tsx
@@ -1,14 +1,21 @@
 import { Stack, StackTypeMap } from "@mui/material"
 import { OverridableComponent } from "@mui/material/OverridableComponent"
+import { ReactNode } from "react"
 
-const VStack = ({ ...props }: OverridableComponent<StackTypeMap<{}, "div">>) => {
+type VStackProps = OverridableComponent<StackTypeMap<{}, "div">> & {
+  children?: ReactNode
+}
+
+const VStack = ({ children, ...props }: VStackProps) => (
   <Stack
     display={'flex'}
     flexDirection={'column'}
     alignItems={'center'}
     justifyContent={'center'}
     {...props}
-  />
-}
+  >
+    {children}
+  </Stack>
+)
 
 export default VStack
